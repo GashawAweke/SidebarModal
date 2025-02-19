9fb6de50316bab1e5113f4b6a812e305e8febc31
GashawAweke <gashaye12aweke@gmail.com>
2025-02-19 06:20:40 +0300

    second commit

---
+++ src/Context.jsx
@@ -0,0 +1,25 @@
+import { Children, createContext, useState } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ Children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return <AppContext.Provider>{Children}</AppContext.Provider>;
+};
+
+export default AppProvider;
